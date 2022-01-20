63fea819e6e4e590ab7679232f5fbc89a42fe83e
Manuel Luemba <mmatosop7@gmail.com>
2022-01-20 15:40:14 +0100

    migrando os meu modelo book para a base de dados

---
+++ app/apps/book/choices.py
@@ -0,0 +1,9 @@
+HARDCOVER = 1
+PAPERBACK = 2
+EBOOK = 3
+
+BOOK_TYPES = (
+    (HARDCOVER, 'Hardcover'),
+    (PAPERBACK, 'Paperback'),
+    (EBOOK, 'E-book'),
+)
--- app/apps/book/models.py
+++ app/apps/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from apps.book.choices import BOOK_TYPES
 
-# Create your models here.
+class Model(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pub_date = models.DateTimeField('date published')
+    upload = models.FileField(upload_to='uploads/%Y/%m/%d/', null=True, blank = True)
+    book_type = models.PositiveSmallIntegerField(choices=BOOK_TYPES, blank=True, null=True)
+    pages = models.IntegerField(blank=True, null=True)
+  
+
