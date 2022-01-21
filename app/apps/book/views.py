f644d0036046abe843fe7e4c7aead9d161aa1b5e
Manuel Luemba <mmatosop7@gmail.com>
2022-01-21 01:16:47 +0100

    fazendo nossa primera vista - booklistview

--- app/apps/book/views.py
+++ app/apps/book/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
-# Create your views here.
-def index(self):
-    return HttpResponse("HOLA MUNDO DJANGO")
+from apps.book.models import Book
+
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book/list.html'
+
