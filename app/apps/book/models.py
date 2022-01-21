c75a146d309972c22833525da74ae526a6b19c83
Manuel Luemba <mmatosop7@gmail.com>
2022-01-21 01:15:55 +0100

    alterando meu modelo

--- app/apps/book/models.py
+++ app/apps/book/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from apps.book.choices import BOOK_TYPES
 
-class Model(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
